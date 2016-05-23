406eb42dd8503b193c54c75f7e33720da3e1c976
alexanderwjrussell <awjrussell@live.co.uk>
2016-05-23 19:17:08 +0100

    More Code Wars

---
+++ NoZerosForHeroes.rb
@@ -0,0 +1,10 @@
+def no_boring_zeros(num)
+  if num % 10 == 0 && num != 0
+    num = num / 10
+    num = no_boring_zeros(num)
+  end
+  num
+end
+
+puts no_boring_zeros(1450)
+puts no_boring_zeros(950000)
